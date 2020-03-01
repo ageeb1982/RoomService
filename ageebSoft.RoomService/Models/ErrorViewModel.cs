518772ad74081b96b9937c8fa1800343ae774f60
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-03-01 22:13:05 +0300

    Add project files.

---
+++ ageebSoft.RoomService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ageebSoft.RoomService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
