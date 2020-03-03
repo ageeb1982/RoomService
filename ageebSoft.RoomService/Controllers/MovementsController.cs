420ab10b5942049fc1a20ced1fe3435f6dbe7a0d
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-03-03 15:06:03 +0300

    update2020_03_03_15h_05

---
+++ ageebSoft.RoomService/Controllers/MovementsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ageebSoft.RoomService.Data;
+using ageebSoft.RoomService.Models;
+
+namespace ageebSoft.RoomService.Controllers
+{
+    public class MovementsController : Controller
+    {
+        private readonly MyDB _context;
+
+        public MovementsController(MyDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Movements
+        public async Task<IActionResult> Index()
+        {
+            var myDB = _context.Movements.Include(m => m.Rooms);
+            return View(await myDB.ToListAsync());
+        }
+
+        // GET: Movements/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.Movements
+                .Include(m => m.Rooms)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+
+            return View(movement);
+        }
+
+        // GET: Movements/Create
+        public IActionResult Create()
+        {
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Id");
+            return View();
+        }
+
+        // POST: Movements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustName,RoomId,Id,Note")] Movement movement)
+        {
+            if (ModelState.IsValid)
+            {
+                movement.Id = Guid.NewGuid();
+                _context.Add(movement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Id", movement.RoomId);
+            return View(movement);
+        }
+
+        // GET: Movements/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.Movements.FindAsync(id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Id", movement.RoomId);
+            return View(movement);
+        }
+
+        // POST: Movements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("CustName,RoomId,Id,Note")] Movement movement)
+        {
+            if (id != movement.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovementExists(movement.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Id", movement.RoomId);
+            return View(movement);
+        }
+
+        // GET: Movements/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.Movements
+                .Include(m => m.Rooms)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+
+            return View(movement);
+        }
+
+        // POST: Movements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var movement = await _context.Movements.FindAsync(id);
+            _context.Movements.Remove(movement);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovementExists(Guid id)
+        {
+            return _context.Movements.Any(e => e.Id == id);
+        }
+    }
+}
